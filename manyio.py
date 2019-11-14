68d77b52751cae2e890c3cf258ac2712ad33868c
Justin Hoppensteadt <justinrocksmadscience@gmail.com>
2019-11-14 17:34:14 +0700

    xmltodict

--- manyio.py
+++ manyio.py
@@ -22,8 +22,9 @@ import xml.etree.ElementTree as ET
 import re
 import yaml
 import json
+import xmltodict
 
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 from pprint import pformat
 from docopt import docopt
 
@@ -37,6 +38,18 @@ except NameError:  # python3
     basestring = str
 
 
+def setup_yaml():
+    """ https://stackoverflow.com/a/8661021 """
+
+    def represent_dict_order(
+        self, data): return self.represent_mapping(
+        'tag:yaml.org,2002:map', data.items())
+    yaml.add_representer(OrderedDict, represent_dict_order)
+
+
+setup_yaml()
+
+
 def debug(message):
     if conf['--debug']:
         print(inspect.stack()[1].function, ":", message)
@@ -45,56 +58,56 @@ def debug(message):
 # these next two are from stackexchange
 
 
-def etree_to_dict(t):
-    d = {t.tag: {} if t.attrib else None}
-    children = list(t)
-    if children:
-        dd = defaultdict(list)
-        for dc in map(etree_to_dict, children):
-            for k, v in dc.items():
-                dd[k].append(v)
-        d = {t.tag: {k: v[0] if len(v) == 1 else v
-                     for k, v in dd.items()}}
-    if t.attrib:
-        d[t.tag].update(('@' + k, v)
-                        for k, v in t.attrib.items())
-    if t.text:
-        text = t.text.strip()
-        if children or t.attrib:
-            if text:
-                d[t.tag]['#text'] = text
-        else:
-            d[t.tag] = text
-    return d
-
-
-def dict_to_etree(d):
-    def _to_etree(d, root):
-        if not d:
-            pass
-        elif isinstance(d, str):
-            root.text = d
-        elif isinstance(d, dict):
-            for k, v in d.items():
-                assert isinstance(k, str)
-                if k.startswith('#'):
-                    assert k == '#text' and isinstance(v, str)
-                    root.text = v
-                elif k.startswith('@'):
-                    assert isinstance(v, str)
-                    root.set(k[1:], v)
-                elif isinstance(v, list):
-                    for e in v:
-                        _to_etree(e, ET.SubElement(root, k))
-                else:
-                    _to_etree(v, ET.SubElement(root, k))
-        else:
-            assert d == 'invalid type', (type(d), d)
-    assert isinstance(d, dict) and len(d) == 1
-    tag, body = next(iter(d.items()))
-    node = ET.Element(tag)
-    _to_etree(body, node)
-    return node
+# def etree_to_dict(t):
+#     d = {t.tag: {} if t.attrib else None}
+#     children = list(t)
+#     if children:
+#         dd = defaultdict(list)
+#         for dc in map(etree_to_dict, children):
+#             for k, v in dc.items():
+#                 dd[k].append(v)
+#         d = {t.tag: {k: v[0] if len(v) == 1 else v
+#                      for k, v in dd.items()}}
+#     if t.attrib:
+#         d[t.tag].update(('@' + k, v)
+#                         for k, v in t.attrib.items())
+#     if t.text:
+#         text = t.text.strip()
+#         if children or t.attrib:
+#             if text:
+#                 d[t.tag]['#text'] = text
+#         else:
+#             d[t.tag] = text
+#     return d
+#
+#
+# def dict_to_etree(d):
+#     def _to_etree(d, root):
+#         if not d:
+#             pass
+#         elif isinstance(d, str):
+#             root.text = d
+#         elif isinstance(d, dict):
+#             for k, v in d.items():
+#                 assert isinstance(k, str)
+#                 if k.startswith('#'):
+#                     assert k == '#text' and isinstance(v, str)
+#                     root.text = v
+#                 elif k.startswith('@'):
+#                     assert isinstance(v, str)
+#                     root.set(k[1:], v)
+#                 elif isinstance(v, list):
+#                     for e in v:
+#                         _to_etree(e, ET.SubElement(root, k))
+#                 else:
+#                     _to_etree(v, ET.SubElement(root, k))
+#         else:
+#             assert d == 'invalid type', (type(d), d)
+#     assert isinstance(d, dict) and len(d) == 1
+#     tag, body = next(iter(d.items()))
+#     node = ET.Element(tag)
+#     _to_etree(body, node)
+#     return node
 
 
 def process_input(input):
@@ -110,7 +123,8 @@ def process_input(input):
     if fmt:
         if re.match(r'^xml', str(fmt), re.I):
             debug("Try to process xml")
-            return(etree_to_dict(ET.fromstring(indata)))
+            return(xmltodict.parse(input))
+            # return(etree_to_dict(ET.fromstring(indata)))
         if re.match(r'^yam?l', str(fmt), re.I):
             debug("Try to process yaml")
             return(yaml.load_all(indata))
@@ -132,7 +146,7 @@ def process_output(fmt, data):
             return(ET.tostringlist(data))
         except AttributeError:
             debug('return(ET.tostringlist(data)) didnt work')
-        return(ET.tostring(dict_to_etree(data)))
+        return(xmltodict.unparse(data))
     if re.match(r'^json', str(fmt), re.I):
         return(json.dumps(data))
     if re.match(r'^pretty', str(fmt), re.I):
@@ -157,7 +171,8 @@ def format_from_filename(f):
 def format_from_data(data):
     try:
         debug('Trying XML')
-        return(etree_to_dict(ET.fromstring(data)))
+        return(xmltodict.parse(data))
+        # return(etree_to_dict(ET.fromstring(data)))
     except BaseException:
         debug('Not XML Data, trying YaML')
     try:
